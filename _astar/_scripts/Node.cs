628e9c0f603604c9c94675d7ff17a8c62a11b24e
1 <1>
2015-02-05 21:06:53 +0800

    1

---
+++ _astar/_scripts/Node.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node {
+	public bool _walkable;
+	public Vector3 _pos;
+	public int _x,_y;
+
+	public int gCost; //与起点距离
+	public int hCost; //与目标点距离
+
+	public int fCost{
+		get{return gCost+hCost;}
+	}
+
+	public Node parent;
+
+	public Node (bool walkable,Vector3 pos,int x,int y){
+		_walkable = walkable;
+		_pos = pos;
+		_x = x;
+		_y = y;
+	}
+}
