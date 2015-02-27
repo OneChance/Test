586d37967bc4734e87f8055a79baf7996e1556af
1 <1>
2015-02-27 15:02:54 +0800

    1

---
+++ A_star/FindPath.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class FindPath : MonoBehaviour
+{
+
+		private Grid _grid;
+		public Transform _player, _target;
+
+		// Use this for initialization
+		void Start ()
+		{
+				_grid = GetComponent<Grid> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				FindingPath (_player.position,_target.position);
+		}
+
+		void FindingPath (Vector3 StartPos, Vector3 EndPos)
+		{
+				Node startNode = _grid.GetFromPosition (StartPos);
+				Node endNode = _grid.GetFromPosition (EndPos);
+
+				List<Node> openSet = new List<Node> ();
+				HashSet<Node> closeSet = new HashSet<Node> ();
+
+				openSet.Add (startNode);
+
+				while (openSet.Count>0) {
+						Node currentNode = openSet [0];
+
+						for (int i=0; i<openSet.Count; i++) {
+								if (openSet [i].fCost < currentNode.fCost || (openSet [i].fCost == currentNode.fCost && openSet [i].hCost < currentNode.hCost)) {
+										currentNode = openSet [i];
+								}
+						}
+				
+						openSet.Remove (currentNode);
+						closeSet.Add (currentNode);
+
+						if (currentNode == endNode) {
+								GeneratePath (startNode, endNode);
+						}
+
+						foreach (Node node in _grid.GetNeibourhood(currentNode)) {
+								if (!node._canWalk || closeSet.Contains (node))
+										continue;
+
+								int newCost = currentNode.gCost + GetDistanceNodes (currentNode, node);
+								if (newCost < node.gCost || !openSet.Contains (node)) {
+										node.gCost = newCost;
+										node.hCost = GetDistanceNodes (node, endNode);
+										node.parent = currentNode;
+										if (!openSet.Contains (node)) {
+												openSet.Add (node);
+										}
+								}
+						}
+				}
+		}
+
+		int GetDistanceNodes (Node a, Node b)
+		{
+				int cntX = Mathf.Abs (a._gridX - b._gridX);
+				int cntY = Mathf.Abs (a._gridY - b._gridY);
+
+				if (cntX > cntY) {
+						return 14 * cntY + 10 * (cntX - cntY);
+				} else {
+						return 14 * cntX + 10 * (cntY - cntX);
+				}
+		}
+
+		void GeneratePath (Node startNode, Node endNode)
+		{
+				List<Node> path = new List<Node> ();
+				Node temp = endNode;
+				while (temp!=startNode) {
+						path.Add (temp);
+						temp = temp.parent;
+				}
+				path.Reverse ();
+				_grid.path = path;
+		}
+
+}
--- A_star/Grid.cs
+++ A_star/Grid.cs
@@ -1,15 +1,106 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using System.Collections;
 
-public class Grid : MonoBehaviour {
+public class Grid : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+		private Node[,] grid;
+		public Vector2 gridSize;
+		public float nodeRadius;
+		private float nodeDiameter;
+		public LayerMask whatLayer;
+		public int gridCntX, gridCntY;
+		public Transform player;
+		public List<Node> path;
+
+		public Node GetFromPosition (Vector3 position)
+		{
+				float percentX = (position.x - transform.position.x + gridSize.x / 2) / gridSize.x;
+				float percentY = (position.z - transform.position.z + gridSize.y / 2) / gridSize.y;
+				
+				percentX = Mathf.Clamp01 (percentX);
+				percentY = Mathf.Clamp01 (percentY);
+
+				int x = Mathf.RoundToInt ((gridCntX - 1) * percentX);
+				int y = Mathf.RoundToInt ((gridCntY - 1) * percentY);
+
+				return grid [x, y];
+		}
+
+		// Use this for initialization
+		void Start ()
+		{
+				nodeDiameter = nodeRadius * 2;
+				gridCntX = Mathf.RoundToInt (gridSize.x / nodeDiameter);
+				gridCntY = Mathf.RoundToInt (gridSize.y / nodeDiameter);
+				grid = new Node[gridCntX, gridCntY];
+
+				CreateGrid ();
+		}
+
+		void CreateGrid ()
+		{
+				Vector3 startPoint = transform.position - gridSize.x / 2 * Vector3.right - gridSize.y / 2 * Vector3.forward;
+				for (int i=0; i<gridCntX; i++) {
+						for (int j=0; j<gridCntY; j++) {
+								Vector3 worldPos = startPoint + Vector3.right * (i * nodeDiameter + nodeRadius) + Vector3.forward * (j * nodeDiameter + nodeRadius);
+								bool walkable = !Physics.CheckSphere (worldPos, nodeRadius, whatLayer);			
+								grid [i, j] = new Node (walkable, worldPos, i, j);
+						}
+				}
+		}
+
+		void OnDrawGizmos ()
+		{
+				Gizmos.DrawWireCube (transform.position, new Vector3 (gridSize.x, 1, gridSize.y));
+				if (grid == null)
+						return;
+			
+				Node playerNode = GetFromPosition (player.position);
+
+				foreach (var node in grid) {
+						Gizmos.color = node._canWalk ? Color.white : Color.red;
+						Gizmos.DrawCube (node._worldPos, Vector3.one * (nodeDiameter - .1f));
+				}
+
+				if (path != null) {
+						foreach (var node in path) {
+								Gizmos.color = Color.black;
+								Gizmos.DrawCube (node._worldPos, Vector3.one * (nodeDiameter - .1f));
+						}
+				}
+
+				if (playerNode != null && playerNode._canWalk) {
+						Gizmos.color = Color.cyan;
+						Gizmos.DrawCube (playerNode._worldPos, Vector3.one * (nodeDiameter - .1f));
+				}
+		}
+
+		public List<Node> GetNeibourhood (Node node)
+		{
+				List<Node> neibourhood = new List<Node> ();
+				for (int i=-1; i<=1; i++) {
+						for (int j=-1; j<=1; j++) {
+								if (i == 0 && j == 0) {
+										continue;
+								}
+				
+								int tempX = node._gridX + i;
+								int tempY = node._gridY + j;
+				
+								if (tempX < gridCntX && tempX > 0 && tempY < gridCntY && tempY > 0) {
+										neibourhood.Add (grid [tempX, tempY]);
+								}
+						}
+				}
+		
+				return neibourhood;
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+		}
 }
